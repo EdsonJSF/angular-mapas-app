1fdb298acb25501da577a33756e6b1d94fba2916
Edsonjsf <edsonjsf1990@gmail.com>
2023-02-23 14:19:37 -0400

    Mostrar un mapa en pantalla completa

--- src/app/mapas/pages/full-screen/full-screen.component.ts
+++ src/app/mapas/pages/full-screen/full-screen.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from '../../../../environments/environment';
+
 @Component({
   selector: 'app-full-screen',
   templateUrl: './full-screen.component.html',
@@ -8,5 +11,13 @@ import { Component, OnInit } from '@angular/core';
 export class FullScreenComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    (mapboxgl as any).accessToken = environment.mapboxToken;
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
 }
