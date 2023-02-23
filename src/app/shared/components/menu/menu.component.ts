036b705a7290e8f36eb55f89c52327ace9ee99f8
Edsonjsf <edsonjsf1990@gmail.com>
2023-02-23 13:53:12 -0400

    Menú de la aplicación

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -1,12 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface MenuItem {
+  path: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
 })
-export class MenuComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class MenuComponent {
+  menuItems: MenuItem[] = [
+    {
+      path: '/mapas/fullscreen',
+      name: 'full screen',
+    },
+    {
+      path: '/mapas/zoom-range',
+      name: 'zoom range',
+    },
+    {
+      path: '/mapas/marcadores',
+      name: 'marcadores',
+    },
+    {
+      path: '/mapas/propiedades',
+      name: 'propiedades',
+    },
+  ];
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './components/menu/menu.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [MenuComponent],
-  imports: [CommonModule],
-  exports: [MenuComponent]
+  imports: [CommonModule, RouterModule],
+  exports: [MenuComponent],
 })
 export class SharedModule {}
