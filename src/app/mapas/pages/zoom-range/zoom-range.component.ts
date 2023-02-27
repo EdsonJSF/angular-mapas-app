65c0351846fa5acef1e726790f7756e992b8dac4
Edsonjsf <edsonjsf1990@gmail.com>
2023-02-27 13:48:15 -0400

    Controlar el objeto del mapa - ZoomIn y ZoomOut

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 
 import * as mapboxgl from 'mapbox-gl';
 
@@ -7,15 +13,28 @@ import * as mapboxgl from 'mapbox-gl';
   templateUrl: './zoom-range.component.html',
   styleUrls: ['./zoom-range.component.scss'],
 })
-export class ZoomRangeComponent implements OnInit {
+export class ZoomRangeComponent implements OnInit, AfterViewInit {
+  @ViewChild('map') divMap!: ElementRef;
+  map!: mapboxgl.Map;
+
   constructor() {}
 
-  ngOnInit(): void {
-    const map = new mapboxgl.Map({
-      container: 'map', // container ID
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.map = new mapboxgl.Map({
+      container: this.divMap.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-72.2321145795547, 7.7681861430432875], // starting position [lng, lat]
       zoom: 17, // starting zoom
     });
+  }
+
+  zoomOut() {
+    this.map.zoomOut();
+  }
+
+  zoomIn() {
+    this.map.zoomIn();
   }
 }
