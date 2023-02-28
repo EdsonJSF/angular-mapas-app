f4fb1b4bcc888c1f042df7ff6ba6adf8409bb9d0
Edsonjsf <edsonjsf1990@gmail.com>
2023-02-28 14:23:49 -0400

    Obtener las coordenadas centrales del mapa y destruccion de los EventListeners

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -2,7 +2,7 @@ import {
   AfterViewInit,
   Component,
   ElementRef,
-  OnInit,
+  OnDestroy,
   ViewChild,
 } from '@angular/core';
 
@@ -13,20 +13,19 @@ import * as mapboxgl from 'mapbox-gl';
   templateUrl: './zoom-range.component.html',
   styleUrls: ['./zoom-range.component.scss'],
 })
-export class ZoomRangeComponent implements OnInit, AfterViewInit {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
   @ViewChild('map') divMap!: ElementRef;
   map!: mapboxgl.Map;
   zoomLevel: number = 10;
+  center: [number, number] = [-72.2321145795547, 7.7681861430432875];
 
   constructor() {}
-
-  ngOnInit(): void {}
 
   ngAfterViewInit(): void {
     this.map = new mapboxgl.Map({
       container: this.divMap.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-72.2321145795547, 7.7681861430432875], // starting position [lng, lat]
+      center: this.center, // starting position [lng, lat]
       zoom: this.zoomLevel,
     });
 
@@ -39,6 +38,18 @@ export class ZoomRangeComponent implements OnInit, AfterViewInit {
         this.map.zoomTo(18);
       }
     });
+
+    this.map.on('move', (event) => {
+      const target = event.target;
+      const { lng, lat } = target.getCenter();
+      this.center = [lng, lat];
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.map.off('zoom', () => {});
+    this.map.off('zoomend', () => {});
+    this.map.off('move', () => {});
   }
 
   zoomOut() {
