6d22fa8364e080e4e155d6f0178913c369ec3d58
Edsonjsf <edsonjsf1990@gmail.com>
2023-03-07 13:24:28 -0400

    Uso del Componente MiniMapa

--- src/app/mapas/components/mini-mapa/mini-mapa.component.ts
+++ src/app/mapas/components/mini-mapa/mini-mapa.component.ts
@@ -1,12 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  AfterViewInit,
+  Input,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
+
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-mini-mapa',
   templateUrl: './mini-mapa.component.html',
   styleUrls: ['./mini-mapa.component.scss'],
 })
-export class MiniMapaComponent implements OnInit {
+export class MiniMapaComponent implements AfterViewInit {
+  @Input() lngLat: [number, number] = [0, 0];
+
+  @ViewChild('map') divMap!: ElementRef;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat, // starting position [lng, lat]
+      zoom: 15, // starting zoom
+      interactive: false,
+    });
+
+    new mapboxgl.Marker().setLngLat(this.lngLat).addTo(map);
+  }
 }
